0e8067bf08305c7402bc4e4b4f622afcc9ccd605
gyeong5961 <gyeong5961@gmail.com>
2021-01-25 18:11:26 +0900

    2021/01/25 chapter 10.2.2

--- src/ch09/styling-react/src/StyledComponent.js
+++ src/ch09/styling-react/src/StyledComponent.js
@@ -1,10 +1,27 @@
 import React from "react";
 import styled, {css} from "styled-components";
 
+const sizes = {
+	desktop: 1024,
+	tablet : 768
+};
+const media = Object.keys(sizes).reduce((acc, label) => {
+	acc[label] = (...args) => css`
+      @media (max-width: ${sizes[label] / 16}em) {
+        ${css(...args)};
+      }`
+	return acc;
+}, {});
+
+
 const Box = styled.div`
-background:${props => props.color||"blue"};
-padding:1rem;
-display:flex;
+  background: ${props => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  ${media.desktop`width:768px;`}
+  ${media.tablet`width:768px;`};
 `;
 
 const Button = styled.button`
@@ -19,20 +36,24 @@ const Button = styled.button`
   font-size: 1rem;
   font-weight: 600;
   /* & 문자를 사용하여 Sass 처럼 자기 자신 선택 가능 */
+
   &:hover {
     background: rgba(255, 255, 255, 0.9);
   }
+
   ${props =>
-	props.inverted &&
-	css`
-      background: none;
-      border: 2px solid white;
-      color: white;
-      &:hover {
-        background: white;
-        color: black;
-      }
-    `};
+          props.inverted &&
+          css`
+            background: none;
+            border: 2px solid white;
+            color: white;
+
+            &:hover {
+              background: white;
+              color: black;
+            }
+          `};
+
   & + button {
     margin-left: 1rem;
   }
---
+++ src/ch10/todo-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+	return (
+		<TodoTemplate>
+			<TodoInsert/>
+		</TodoTemplate>
+	);
+};
+
+export default App;
