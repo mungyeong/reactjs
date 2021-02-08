45da6f113fef271665762a6f0000074846e7fb69
gyeong5961 <gyeong5961@gmail.com>
2021-02-08 17:28:00 +0900

    2021/02/08 chapter 21 succeeded

--- src/ch21/blog/blog-backend/src/api/index.js
+++ src/ch21/blog/blog-backend/src/api/index.js
@@ -3,6 +3,7 @@ const posts = require("./posts");
 
 const api = new Router();
 
+
 api.use("/posts", posts.routes());
 
 module.exports = api;
--- src/ch21/blog/blog-backend/src/index.js
+++ src/ch21/blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
 
 const api = require("./api")
 
@@ -8,19 +9,7 @@ const router = new Router();
 
 router.use("/api", api.routes());
 
-router.get("/", ctx => {
-	ctx.body = "홈";
-});
-
-router.get("/about/:name?", ctx => {
-	const {name} = ctx.params;
-	ctx.body = name ? `${name}의 소개` : "소개";
-});
-
-router.get("/posts", ctx => {
-	const {id} = ctx.query;
-	ctx.body = id ? `포스트 #${id}` : "포스트 아이디가 없습니다.";
-});
+app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
